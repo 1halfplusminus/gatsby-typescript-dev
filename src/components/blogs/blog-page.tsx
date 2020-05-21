7e94b57fcace6def6d9b9273b3fc7002578d5e40
1halfplusminus <gael.sautron@si2p.nc>
2020-05-21 14:19:02 +1100

    Feat - Feat fix import

--- src/components/blogs/blog-page.tsx
+++ src/components/blogs/blog-page.tsx
@@ -1,18 +1,18 @@
-import React, { PropsWithChildren } from "react";
-import { Title3 } from "../../theme/title";
+import React, { PropsWithChildren } from "react"
+import { Title3 } from "../core/title"
 
 export interface BlogPageProps {
-  title: string;
-  subtitle: string;
-  excerpt: string;
+  title: string
+  subtitle: string
+  excerpt: string
 }
 
 export const BlogPage = ({ title, subtitle, excerpt }: BlogPageProps) => {
-  return <BlogPage.Wrapper />;
-};
+  return <BlogPage.Wrapper />
+}
 BlogPage.Wrapper = ({ children }: PropsWithChildren<{}>) => {
-  return <>{children}</>;
-};
+  return <>{children}</>
+}
 BlogPage.PageTitle = ({ title }: { title: string }) => {
-  return <Title3>{title} </Title3>;
-};
+  return <Title3>{title} </Title3>
+}
